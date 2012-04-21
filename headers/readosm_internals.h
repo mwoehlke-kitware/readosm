d0b524612163d7b8e7eff144caf5a08a55bfa8f3
sandro <sandro>
2012-04-21 19:37:10 +0000

    reorganizing the code into many separate sources and headers

--- headers/readosm_internals.h
+++ headers/readosm_internals.h
@@ -44,6 +44,10 @@
 / 
 */
 
+/* endianness */
+#define READOSM_BIG_ENDIAN	0
+#define READOSM_LITTLE_ENDIAN	1
+
 /* magic signatures */
 #define READOSM_MAGIC_START	945371767
 #define READOSM_MAGIC_END	1472954381
@@ -57,68 +61,6 @@
 #define READOSM_CURRENT_TAG_IS_NODE	101
 #define READOSM_CURRENT_TAG_IS_WAY	102
 #define READOSM_CURRENT_TAG_IS_RELATION	103
-
-/*
- * PBF stands for: Protocol Buffer Format
- * a serialization notation documented here:
- * http://wiki.openstreetmap.org/wiki/PBF_Format
- * and here too:
- * https://developers.google.com/protocol-buffers/docs/encoding
- */
-
-/* PBF endianness */
-#define READOSM_BIG_ENDIAN	0
-#define READOSM_LITTLE_ENDIAN	1
-
-/* PBF variant validity */
-#define READOSM_VAR_INVALID	0
-#define READOSM_VAR_VALID	1
-
-/* PBF variant types */
-#define READOSM_VAR_UNDEFINED	0
-#define READOSM_VAR_INT32	1
-#define READOSM_VAR_INT64	2
-#define READOSM_VAR_UINT32	3
-#define READOSM_VAR_UINT64	4
-#define READOSM_VAR_SINT32	5
-#define READOSM_VAR_SINT64	6
-#define READOSM_VAR_BOOL	7
-#define READOSM_VAR_ENUM	8
-#define READOSM_LEN_BYTES	9
-
-/* PBF bitmasks used for 32 bit VarInts */
-#define READOSM_MASK32_1	0x0000007f
-#define READOSM_MASK32_2	0x00003f80
-#define READOSM_MASK32_3	0x001fc000
-#define READOSM_MASK32_4	0x0fe00000
-#define READOSM_MASK32_5	0xf0000000
-
-/* PBF bitmasks used for 64 bit VarInts */
-#define READOSM_MASK64_1	0x000000000000007f
-#define READOSM_MASK64_2	0x0000000000003f80
-#define READOSM_MASK64_3	0x00000000001fc000
-#define READOSM_MASK64_4	0x000000000fe00000
-#define READOSM_MASK64_5	0x00000007f0000000
-#define READOSM_MASK64_6	0x000003f800000000
-#define READOSM_MASK64_7	0x0001fc0000000000
-#define READOSM_MASK64_8	0x00fe000000000000
-#define READOSM_MASK64_9	0x7f00000000000000
-#define READOSM_MASK64_A	0x8000000000000000
-
-typedef struct readosm_packed_infos_struct
-{
-/* a struct supporting DenseInfos parsing */
-    int ver_count;		/* how many Version values are there */
-    int *versions;		/* Version values array */
-    int tim_count;		/* how many Timestamp values are there */
-    int *timestamps;		/* Timestamp values array */
-    int cng_count;		/* how many Changeset values are there */
-    long long *changesets;	/* Changeset values array */
-    int uid_count;		/* how many UID values are there */
-    int *uids;			/* UID values array */
-    int usr_count;		/* how many User values are there */
-    int *users;			/* indexes to access corresponding Strings in StringTable */
-} readosm_packed_infos;
 
 typedef struct readosm_internal_tag_struct
 {
@@ -272,109 +214,6 @@ typedef union readosm_endian8_union
     double double_value;	/* floating point, double precision */
 } readosm_endian8;
 
-typedef struct readosm_variant_hint_struct
-{
-/* VariantHints for PBF parsing */
-    unsigned char field_id;	/* field ID */
-    unsigned char type;		/* expected type */
-    struct readosm_variant_hint_struct *next;	/* supporting linked list */
-} readosm_variant_hint;
-
-typedef struct readosm_variant_struct
-{
-/* a PBF Variant type wrapper */
-    char little_endian_cpu;	/* actual CPU endianness */
-    unsigned char type;		/* current type */
-    unsigned char field_id;	/* field ID */
-    union variant_value
-    {
-	int int32_value;	/* signed int32 */
-	unsigned int uint32_value;	/* unsigned int32 */
-	long long int64_value;	/* signed int64 */
-	unsigned long long uint64_value;	/* unsigned int64 */
-	float float_value;	/* floating point, single precision */
-	double double_value;	/* floating point, double precision */
-    } value;			/* numeric variant value */
-    size_t length;		/* length in bytes [for strings] */
-    unsigned char *pointer;	/* pointer to String value */
-    char valid;			/* valid value */
-    readosm_variant_hint *first;	/* pointers supporting a linked list */
-    readosm_variant_hint *last;	/* of VariantHints items */
-} readosm_variant;
-
-typedef struct readosm_string_struct
-{
-/* a String into a PBF StringTable */
-    char *string;		/* pointer to string value (NULL terminated string) */
-    struct readosm_string_struct *next;	/*  supporting linked list */
-} readosm_string;
-
-typedef struct readosm_string_table_struct
-{
-/* a PBF StringTable
- / 
- / Remark: each PBF compressed block includes a StringTable
- / i.e. a centralized table where any string value used within
- / the compressed block itself appears only one time.
- / This is obviously intended so to minimize storage requirements.
- /
- / Individual objects within the PBF file will never directly
- / encode string values; they'll use instead the corresponding
- / index referencing the appropriate string within the StringTable.
-*/
-    readosm_string *first;	/* pointers supporting a linked list */
-    readosm_string *last;	/* of PBF string objects */
-    int count;			/* how many TAG items are there */
-    readosm_string **strings;	/* array of PBF string objects */
-} readosm_string_table;
-
-typedef struct readosm_uint32_struct
-{
-/* a uint32 value into the PBF packed object */
-    unsigned int value;
-    struct readosm_uint32_struct *next;
-} readosm_uint32;
-
-typedef struct readosm_uint32_packed_struct
-{
-/* a PBF uint32 packed object */
-    readosm_uint32 *first;
-    readosm_uint32 *last;
-    int count;
-    unsigned int *values;
-} readosm_uint32_packed;
-
-typedef struct readosm_int32_struct
-{
-/* an int32 value into the PBF packed object */
-    int value;
-    struct readosm_int32_struct *next;
-} readosm_int32;
-
-typedef struct readosm_int32_packed_struct
-{
-/* a PBF int32 packed object */
-    readosm_int32 *first;
-    readosm_int32 *last;
-    int count;
-    int *values;
-} readosm_int32_packed;
-
-typedef struct readosm_int64_struct
-{
-/* an int64 value into the PBF packed object */
-    long long value;
-    struct readosm_int64_struct *next;
-} readosm_int64;
-
-typedef struct readosm_int64_packed_struct
-{
-/* a PBF int64 packed object */
-    readosm_int64 *first;
-    readosm_int64 *last;
-    int count;
-    long long *values;
-} readosm_int64_packed;
 
 typedef struct readosm_file_struct
 {
@@ -385,3 +224,32 @@ typedef struct readosm_file_struct
     char little_endian_cpu;	/* actual CPU endianness */
     int magic2;			/* magic signature #2 */
 } readosm_file;
+
+/* functions handling common OSM objects */
+readosm_internal_tag *alloc_internal_tag (void);
+void destroy_internal_tag (readosm_internal_tag * tag);
+readosm_internal_ref *alloc_internal_ref (void);
+void destroy_internal_ref (readosm_internal_ref * ref);
+readosm_internal_member *alloc_internal_member (void);
+void destroy_internal_member (readosm_internal_member * member);
+void init_internal_node (readosm_internal_node * node);
+void append_tag_to_node (readosm_internal_node * node, const char *key, const char *value);
+void destroy_internal_node (readosm_internal_node * node);
+readosm_internal_way *alloc_internal_way (void);
+void append_reference_to_way (readosm_internal_way * way, long long node_ref);
+void append_tag_to_way (readosm_internal_way * way, const char *key, const char *value);
+void destroy_internal_way (readosm_internal_way * way);
+readosm_internal_relation *alloc_internal_relation (void);
+void append_member_to_relation (readosm_internal_relation * relation, int type, long long id, const char *role);
+void append_tag_to_relation (readosm_internal_relation * relation, const char *key, const char *value);
+void destroy_internal_relation (readosm_internal_relation * relation);
+
+/* XML and ProtoBuf parsers */
+int parse_osm_pbf (readosm_file * input, const void *user_data, readosm_node_callback node_fnct, readosm_way_callback way_fnct, readosm_relation_callback relation_fnct);
+int parse_osm_xml (readosm_file * input, const void *user_data, readosm_node_callback node_fnct, readosm_way_callback way_fnct, readosm_relation_callback relation_fnct);
+
+/* callback handlers */
+int call_node_callback (readosm_node_callback node_callback, const void *user_data, readosm_internal_node * node);
+int call_way_callback (readosm_way_callback way_callback, const void *user_data, readosm_internal_way * way);
+int call_relation_callback (readosm_relation_callback relation_callback, const void *user_data, readosm_internal_relation * relation);
+
